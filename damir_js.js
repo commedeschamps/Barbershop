ec1123530808a49ef08f07adb4c8b3cb340b0448
Damir Zarkesh <240088@astanait.edu.kz>
2025-10-14 12:11:21 +0500

    Damir

--- damir_js.js
+++ damir_js.js
@@ -22,7 +22,7 @@ function change_bg_color(el){
 
 let date = document.getElementById("pr_date").innerHTML = "Date: <br>" + Date();
 
-document.getElementById('purchase_form_id').addEventListener('submit', checkSubmit)
+document.getElementById('purchase_form_id').addEventListener("submit", checkSubmit)
 
 function checkSubmit(event){
     event.preventDefault();
@@ -32,7 +32,28 @@ function checkSubmit(event){
     let cvv = el.cvv.value;
     let date_purchase = el.date_purchase.value;
 
-    
+    let fail = "";
+
+    if(cardNumber == "" || cvv == ""){
+        fail = "Fil all field!";
+        // document.getElementById('cardNumber').innerHTML = error;
+        // document.getElementById('cvv').innerHTML = error;
+        // document.getElementById('date_purchase').innerHTML = error;
+    }else if(cardNumber < 16 || /\p{L}/u.test(cardNumber)){
+        fail = "Fill card number correctly!";
+        // document.getElementById('cardNumber').innerHTML = error;
+    }else if(cvv.length > 3 || /\p{L}/u.test(cvv)){
+        fail = "CVV is not correct!";
+        // document.getElementById('cvv').innerHTML = error;
+    }
+
+    if(fail != ""){
+        document.getElementById('fail').innerHTML = fail;
+    }else{
+        alert("Succesfully bought!");
+    }
+
+
 
 }
 

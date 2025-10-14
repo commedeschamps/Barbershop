0dbf381e8ebc9ef85a7d4edbc5d3c411bf499272
Damir Zarkesh <240088@astanait.edu.kz>
2025-10-14 18:35:48 +0500

    Damir

--- damir_js.js
+++ damir_js.js
@@ -6,14 +6,14 @@ menu_pr.onclick = () => {
     navbar_pr.classList.toggle('show'); 
 }
         
-const myButton = document.querySelector('.nav_item_pr');
+// const myButton = document.querySelector('.nav_item_pr');
 
-myButton.addEventListener('click', () => {
-    myButton.classList.toggle('active');
-});
+// myButton.addEventListener('clicked', () => {
+//     myButton.classList.toggle('active');
+// });
     
 
-let button = false;
+
 
 function change_bg_color(el){
     el.style.background = "#2a2a2a";
@@ -25,9 +25,8 @@ let date = document.getElementById("pr_date").innerHTML = "Date: <br>" + Date();
 document.getElementById('purchase_form_id').addEventListener("submit", checkSubmit)
 
 function checkSubmit(event){
-    event.preventDefault();
     let el = document.getElementById('purchase_form_id');
-
+    el.preventDefault();
     let cardNumber = el.cardNumber.value;
     let cvv = el.cvv.value;
     // let date_purchase = el.date_purchase.value;
@@ -39,22 +38,23 @@ function checkSubmit(event){
         // document.getElementById('cardNumber').innerHTML = error;
         // document.getElementById('cvv').innerHTML = error;
         // document.getElementById('date_purchase').innerHTML = error;
-    }else if(cardNumber < 16 || /\p{L}/u.test(cardNumber)){
+    }
+    else if(cardNumber.length < 16 || /\p{L}/u.test(cardNumber)){
         fail = "Fill card number correctly!";
         // document.getElementById('cardNumber').innerHTML = error;
-    }else if(cvv.length > 3 || /\p{L}/u.test(cvv)){
+    }
+    else if(cvv.length > 3 || /\p{L}/u.test(cvv)){
         fail = "CVV is not correct!";
+        
         // document.getElementById('cvv').innerHTML = error;
     }
 
     if(fail != ""){
         document.getElementById('fail').innerHTML = fail;
+        
     }else{
         alert("Succesfully bought!");
     }
-
-
-
 }
 
 
